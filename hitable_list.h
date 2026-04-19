bf687cc6816d06ef593f115936b15c59a4325252
sumeettummarmatti <sumeettummarmatti@gmail.com>
2026-04-20 00:23:16 +0530

    Fix execution space mismatch in hitable_list.

--- hitable_list.h
+++ hitable_list.h
@@ -5,14 +5,14 @@
 
 class hitable_list: public hitable  {
     public:
-        __device__ hitable_list() {}
-        __device__ hitable_list(hitable **l, int n) {list = l; list_size = n; }
-        __device__ virtual bool hit(const ray& r, float tmin, float tmax, hit_record& rec) const;
+        __host__ __device__ hitable_list() {}
+        __host__ __device__ hitable_list(hitable **l, int n) {list = l; list_size = n; }
+        __host__ __device__ virtual bool hit(const ray& r, float tmin, float tmax, hit_record& rec) const;
         hitable **list;
         int list_size;
 };
 
-__device__ bool hitable_list::hit(const ray& r, float t_min, float t_max, hit_record& rec) const {
+__host__ __device__ bool hitable_list::hit(const ray& r, float t_min, float t_max, hit_record& rec) const {
         hit_record temp_rec;
         bool hit_anything = false;
         float closest_so_far = t_max;
