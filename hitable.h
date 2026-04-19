a468a2008b6fb6014b25979a0d257b213cdb12f7
sumeettummarmatti <sumeettummarmatti@gmail.com>
2026-04-19 23:37:20 +0530

    Standardize host/device decorators across core headers.

--- hitable.h
+++ hitable.h
@@ -25,7 +25,7 @@ struct hit_record
 
 class hitable  {
     public:
-        __device__ virtual bool hit(const ray& r, float t_min, float t_max, hit_record& rec) const = 0;
+        __host__ __device__ virtual bool hit(const ray& r, float t_min, float t_max, hit_record& rec) const = 0;
 };
 
 #endif
--- ray.h
+++ ray.h
@@ -5,11 +5,11 @@
 class ray
 {
     public:
-        __device__ ray() {}
-        __device__ ray(const vec3& a, const vec3& b) { A = a; B = b; }
-        __device__ vec3 origin() const       { return A; }
-        __device__ vec3 direction() const    { return B; }
-        __device__ vec3 point_at_parameter(float t) const { return A + t*B; }
+        __host__ __device__ ray() {}
+        __host__ __device__ ray(const vec3& a, const vec3& b) { A = a; B = b; }
+        __host__ __device__ vec3 origin() const       { return A; }
+        __host__ __device__ vec3 direction() const    { return B; }
+        __host__ __device__ vec3 point_at_parameter(float t) const { return A + t*B; }
 
         vec3 A;
         vec3 B;
